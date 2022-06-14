62c4b453e5a0417022e5284a4137fa2e5fd47ba0
derja12 <derekjacketta@gmail.com>
2022-06-14 11:46:51 -0600

    finished magic eight ball

--- Day-2/Project01-Magic-Eight-Ball/app.js
+++ Day-2/Project01-Magic-Eight-Ball/app.js
@@ -1,16 +1,21 @@
+const URL = "https://cs2022-eight-ball.herokuapp.com";
+
 var app = new Vue({
     el: "#app",
     data: {
         prompt: "Ask a question",
         message: "",
-        messageBank: [
-            "Yes",
-            "No",
-            "Maybe",
-            "Ask Again Tomorrow",
-            "Probably",
-            "Probably Not"
-        ],
+        messageColor: "",
+        answerReady: true,
+        gradient: "",
+        // messageBank: [
+        //     "Yes",
+        //     "No",
+        //     "Maybe",
+        //     "Ask Again Tomorrow",
+        //     "Probably",
+        //     "Probably Not"
+        // ],
 
         question: "",
     },
@@ -18,9 +23,26 @@ var app = new Vue({
         askQuestion: function () {
             if (!this.isValidQuestion()) {return;}
 
-            let nextIndex = Math.floor(Math.random() * this.messageBank.length);
-            let nextResponse = this.messageBank[nextIndex];
-            this.message = nextResponse;
+            // let nextIndex = Math.floor(Math.random() * this.messageBank.length);
+            // let nextResponse = this.messageBank[nextIndex];
+            // this.message = "";
+            this.answerReady = false;
+            this.messageColor = "rgba(255,255,255,0)";
+            setTimeout(() => {
+                fetch(URL + "/questions").then((response) => {
+                    response.json().then((data) => {
+                        let answerObject = data;
+    
+                        this.message = answerObject.answer;
+                        this.messageColor = answerObject.color;
+                        this.gradient = "linear-gradient(0deg, rgba(0,0,0,1) 0%, " + answerObject.color + " 50%)";
+                        setTimeout(() => {
+                            this.answerReady = true;
+                        }, 1000);
+                    });
+                });
+            }, 1500);
+            //this.message = nextResponse;
             this.prompt = "Ask another question";
             this.question = "";
         },
@@ -29,4 +51,4 @@ var app = new Vue({
         }
     }
 
-})
+});
