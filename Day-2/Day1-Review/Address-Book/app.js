c698992352691ef8e57b214ae11a482eb43bd397
derja12 <derekjacketta@gmail.com>
2022-06-13 20:54:41 -0600

    Day-2 starter files

---
+++ Day-2/Day1-Review/Address-Book/app.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        // variables to model each input
+
+        // list to hold addresses
+    },
+    methods: {
+        // function to add(push) address into list, clear input fields
+        recordAddress: function () {
+            
+        }
+    }
+})
---
+++ Day-2/Day1-Review/Whack-a-Mole/app.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        // row count and column count
+
+        // mole position (row and column)
+
+        // score and total
+    },
+    methods: {
+        updateMoleRecursive: function () {
+            // use SetTimeout to 
+            // 1. change the mole's position
+            // 2. update the total
+            
+        },
+        hitMole: function () {
+            // 1. add 1 to the score
+
+            // 2. clear the mole off the board
+
+        }
+    },
+    created: function () {
+        // Call Recursive function to start
+    }
+});
