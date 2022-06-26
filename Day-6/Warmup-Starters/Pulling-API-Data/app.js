1e1536f9db85b57366d6ba308f5f22eff0c4c473
derja12 <derekjacketta@gmail.com>
2022-06-25 21:57:38 -0600

    Day 6 initial

---
+++ Day-6/Warmup-Starters/Pulling-API-Data/app.js
@@ -0,0 +1,29 @@
+const API_URL = "https://fakestoreapi.com";
+
+var app = new Vue({
+
+});
+
+
+
+
+/* 
+
+=== Instructions ===
+
+1. Get the Vue <script> tag hooked up 
+    (Can be found on v2.vuejs.org under 'Get Started')
+
+2. Fetch the API resource 'products' (GET request on https://fakestoreapi.com/products)
+    2.1 Use Postman or console.log() to see what the data looks like
+
+3. Use a v-for to display each item in the list
+    3.1 Display the following: image, title, description and price
+
+
+=== If you've finished the above ===
+
+4. Fetch the data using 'async/await' instead of '.then()' 
+    (look up javascript async/await)
+
+*/
