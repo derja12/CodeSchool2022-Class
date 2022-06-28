c1561c36b0839c935bf3f3290e56267718c517c7
derja12 <derekjacketta@gmail.com>
2022-06-28 13:39:30 -0600

    Day 7 finished

--- Day-7/Warmup-Starters/Address-Book-Components/app.js
+++ Day-7/Warmup-Starters/Address-Book-Components/app.js
@@ -1,16 +1,48 @@
-Vue.component('address', {
-    template: ``,
-    props: []   // or {}
+Vue.component('custom-address', {
+    template: `
+    <div>
+        <h3>{{ address.name }}</h3>
+        <p>{{ address.address }}</p>
+        <button v-on:click='removeAddress'>Remove</button>
+    </div>
+    `,
+    props: {
+        'address': Object,
+        'index': Number,
+        'list': Array
+    },
+    methods: {
+        removeAddress: function () {
+            this.list.splice(this.index, 1);
+        }
+    }
 })
 
 var app = new Vue({
     el: "#app",
     data: {
         // variables to model each input
+        name: "",
+        address: "",
 
         // list to hold addresses
+        addressList: []
     },
     methods: {
         // function to push address into list, clear input fields
+        createAddress: function () {
+            // create new address object
+            let newAddress = {
+                name: this.name,
+                address: this.address
+            }
+            
+            // push on the new address
+            this.addressList.push(newAddress);
+
+            // clear the inputs
+            this.name = "";
+            this.address = "";
+        }
     }
 })
