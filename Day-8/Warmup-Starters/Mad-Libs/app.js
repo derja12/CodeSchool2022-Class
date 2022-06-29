5d8176cb94c134e9acf9912f6786771a2b7e9a7f
derja12 <derekjacketta@gmail.com>
2022-06-29 13:14:21 -0600

    after day 8

--- Day-8/Warmup-Starters/Mad-Libs/app.js
+++ Day-8/Warmup-Starters/Mad-Libs/app.js
@@ -1,7 +1,7 @@
 // Default Mad Lib component
 Vue.component("pessimistic-mad-lib", {
     template: `
-    <div id='story'>
+    <div class='story'>
         <b>{{ name }}</b> was born <b>{{ birthday }}</b> and is absolutely frightened at the idea of eating <b>{{ favoriteFood }}</b>.
         All good things come in <b>{{favoriteNumber}}</b>'s, right? Not according to <b>{{ name }}</b>. That's how many times <b>{{ name }}</b> has been
         able to buy <b>{{ favoriteShoppingItem }}</b> - clearly not enough! What's more, <b>{{ name }}</b> found out that
@@ -20,9 +20,44 @@ Vue.component("pessimistic-mad-lib", {
 });
 
 // Implement your own mad lib component here:
-
+Vue.component("optimistic-mad-lib", {
+    template: `
+    <div class='story'>
+        <b>{{ name }}</b> is a pretty cool person. 
+        They enjoy making their favorite food, <b>{{ favoriteFood }}</b>,
+        eating it every <b>{{ birthday }}</b>.
+        <b>{{ name }}</b> have saved the world probably around <b>{{ favoriteNumber }}</b> times,
+        just like their heroes in their favorite film - <b>{{ favoriteMovie }}</b>.
+        On weekends they go looking for <b>{{ favoriteShoppingItem }}</b>, willing to drive for hours
+        till one is found.
+    </div>
+    `,
+    props: {
+        'name': String,
+        'birthday': String,
+        'favoriteFood': String,
+        'favoriteNumber': Number,
+        'favoriteMovie': String,
+        'favoriteShoppingItem': String,
+    },
+    data: function () {
+        return {
+            nameInput: ""
+        }
+    }
+});
 
 // Minimum Vue Instance
 var app = new Vue({
     el: "#app",
+    data: {
+        nameInput: "",
+        birthdayInput: "",
+        favoriteFoodInput: "",
+        favoriteNumberInput: "",
+        favoriteMovieInput: "",
+        favoriteShoppingItemInput: "",
+
+        page: "inputs"
+    }
 });
