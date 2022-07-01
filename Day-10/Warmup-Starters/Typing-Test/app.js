2dda2ae89b00b1edfd22543f0d29927ddf1b8ef1
derja12 <derekjacketta@gmail.com>
2022-06-30 22:29:38 -0600

    day 10 initial

---
+++ Day-10/Warmup-Starters/Typing-Test/app.js
@@ -0,0 +1,47 @@
+const SENTENCES = [
+    "The quick brown fox jumped over the fence.",
+    "It's been over a fence, I'm starting to think that tractor is never coming back.",
+    "What do you believe to be the answer to my problems?",
+]
+
+var app = new Vue({
+    el: "#app",
+    data:{
+    },
+    methods:{
+        startRace: function () {
+        },
+        getRandomSentence: function () {
+        },
+        calculateWordsPerSecond: function () {
+        },
+        resetTest: function () {
+        },
+        resetWithNewSentence: function () {
+        }
+    },
+    computed: {
+        // this function runs whenever the sentence the user is typing changes
+        // use it like a variable (v-if="finishedTyping")
+        finishedTyping: function () {
+            // you probably wanna use your variable here in place of these awful ones
+            if (this.SENTENCE_TO_BE_TYPED == this.SENTENCE_USER_IS_TYPING) {
+                this.calculateWordsPerSecond();
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    created: function () {
+    }
+});
+
+/*
+
+extra cool additions:
+ - Keep a high score that stays between attempts
+ - Let the user pick the sentence that they will be typing
+ - Show a timer on the screen as they are typing
+
+*/
